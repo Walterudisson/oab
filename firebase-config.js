3d48cb1e3b5286a41ac108d8f41739b3637a3092
Walterudisson <101980475+Walterudisson@users.noreply.github.com>
2026-09-25 14:35:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,10 +5,10 @@
 // deve ser feita com Authentication + Security Rules (+ App Check quando formos publicar).
 
 export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyD692T4-gKjtjHKzpFptPiiJjZeAx-VbJc",
+  authDomain: "oabcaderno.firebaseapp.com",
+  projectId: "oabcaderno",
+  storageBucket: "oabcaderno.firebasestorage.app",
+  messagingSenderId: "850432407887",
+  appId: "1:850432407887:web:cbbe40be1d0ee734e56d3a"
 };
